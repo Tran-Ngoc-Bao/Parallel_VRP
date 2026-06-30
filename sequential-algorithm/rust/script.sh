3bd30cc5224bbdc892399df666bd25ff6ca52378
Tran-Ngoc-Bao <tranngocbaooooo12062003@gmail.com>
2026-06-30 10:27:45 +0000

    add more baseline results

--- sequential-algorithm/rust/script.sh
+++ sequential-algorithm/rust/script.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 BIN="${SCRIPT_DIR}/target/release/sequence-tabu-search"
 
-DEFAULT_DATA_PREFIX="200"
+DEFAULT_DATA_PREFIX="50"
 PROBLEM_FILE="${1:-${SCRIPT_DIR}/../../data/soict-2025/${DEFAULT_DATA_PREFIX}.40.2.txt}"
 
 "${BIN}" run \
